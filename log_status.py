6642e8ced4d4c51798ace170755758fa6fdf9fef
Sathusan Rajanikanth <97081481+Rj-Sathusan@users.noreply.github.com>
2025-03-13 19:53:48 +0530

    Update log_status.py

--- log_status.py
+++ log_status.py
@@ -5,7 +5,7 @@ def log_status(website, status):
         log_file.write(f"{website} - Status: {status}\n")
     print(f"📌 Logged: {website} - {status}")
 
-i __name__ == "__main__":
+if __name__ == "__main__":
     if len(sys.argv) == 3:
         log_status(sys.argv[1], sys.argv[2])
     else:
